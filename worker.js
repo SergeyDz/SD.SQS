4ed6bfacd6e2ebfda949ae0908cd8d9a2197ab15
SergeyDzIntapp <sergey.dziuban@intapp.com>
2016-05-30 19:01:54 +0300

    Direct exchange by action name added

--- worker.js
+++ worker.js
@@ -11,11 +11,11 @@ var args = process.argv.slice(2);
 log.info('started');
 log.info(args[2]);
 
- 
+
 var queue = sqs({
 	access: args[0],
 	secret: args[1],
-	region: args[2] // defaults to us-east-1 
+	region: args[2] // defaults to us-east-1
 });
 
 function bail(err) {
@@ -26,24 +26,24 @@ function bail(err) {
 function pullSQS(rabbit)
 {
     queue.pull(args[3], function(message, callback) {
-        log.info(message);  
-        
-        var payload = { 
-          EventType: message.EventType,  
+        log.info(message);
+
+        var payload = {
+          EventType: message.EventType,
           EventId: message.EventId,
           TenantId: message.TenantId,
           Action: message.Payload.Name,
           CorrelationId: message.CorrelationId,
-          Entity: 
+          Entity:
           {
             Id: message.Payload.Source.EntityId,
             Name: message.Payload.Source.Name
           }
       };
-        
-        rabbit.sendToQueue(q, new Buffer(JSON.stringify(payload)));
-        callback(); // we are done with this message - pull a new one 
-                    // calling the callback will also delete the message from the queue 
+
+        rabbit.publish(q, message.Payload.Name, new Buffer(JSON.stringify(payload)));
+        callback(); // we are done with this message - pull a new one
+                    // calling the callback will also delete the message from the queue
     });
 }
 
@@ -54,7 +54,7 @@ amqplib.connect(mq, function(err, conn) {
       conn.createChannel(on_open);
       function on_open(err, ch) {
         if (err != null) bail(err);
-        ch.assertQueue(q);
-        pullSQS(ch); 
+        ch.assertExchange(q, 'direct', { durable: false });
+        pullSQS(ch);
       }
 });
